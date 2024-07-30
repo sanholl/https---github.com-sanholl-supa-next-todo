796ce74bfed1934d2f495951f66d588aefa43410
sanholl <edgar.jang93@hanmail.net>
2024-07-31 00:16:04 +0900

    feat: create todo list with supabase & next

---
+++ app/todo-no-rls/page.tsx
@@ -0,0 +1,15 @@
+import { sleep } from '@/lib/utils';
+import React from 'react';
+import TodoContainer from './components/TodoContainer';
+
+const Page = async () => {
+
+  
+  return (
+    <div>
+      <TodoContainer />
+    </div>
+  );
+}
+
+export default Page;
